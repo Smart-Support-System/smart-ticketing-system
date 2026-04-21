19484b102409be586f03f25cff34b2f853dd8376
IsaiahByrd <isaiah.byrd210@gmail.com>
2026-04-21 12:46:14 -0400

    fix: update user_id to SERIAL and enhance timestamp fields with time zone. assignee_id changed to not delete all tickets when agent is deleted.

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS users(
-    user_id INTEGER PRIMARY KEY,
+    user_id INTEGER SERIAL PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
     name TEXT,
     password_hash BYTEA,
     is_approved BOOLEAN
@@ -34,12 +35,12 @@ CREATE TABLE IF NOT EXISTS tickets(
     ticket_id INTEGER PRIMARY KEY,
     title TEXT,
     description TEXT,
-    created_date TIMESTAMP,
+    created_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     ticket_priority PRIORITY,
     ticket_status STATUS,
     ticket_category_id INTEGER REFERENCES ticket_categories(category_id) ON DELETE SET NULL ON UPDATE CASCADE,
     user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    assignee_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+    assignee_id INTEGER REFERENCES users(user_id) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS ticket_updates(
@@ -48,6 +49,6 @@ CREATE TABLE IF NOT EXISTS ticket_updates(
     ticket_status STATUS,
     message TEXT,
     user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    time TIMESTAMP
+    time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
