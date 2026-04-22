52c779e9e0d225c2bf24394d699316e9438f92a4
Isaiah Byrd <isaiah.byrd210@gmail.com>
2026-04-21 22:35:20 -0400

    Feature/authentication 1 (#3)
    * add docker-compose configuration for PostgreSQL database
    
    * feat: add PostgreSQL support with TypeORM and configuration management
    
    - Updated package.json to include @nestjs/config, @nestjs/typeorm, pg, and typeorm dependencies.
    - Configured ConfigModule for global access to environment variables.
    - Set up TypeOrmModule for PostgreSQL connection using ConfigService for dynamic configuration.
    - Enabled auto-loading of entities and synchronization for development purposes.
    
    * feat: initial implementation of session authentication
    
    * fix: added auth to app, uncommented postgres
    
    * fix: fix broken auth endpoint due to local strategy missing in app.module.ts
    
    * feat: added initial sql schema for project (#1)
    
    * feat: added initial sql schema for project
    
    * fix: switch password hash to binary value
    
    * fix: update user_id to SERIAL and enhance timestamp fields with time zone. assignee_id changed to not delete all tickets when agent is deleted.
    
    ---------
    
    Co-authored-by: IsaiahByrd <isaiah.byrd210@gmail.com>
    
    * fix: add volume for database initialization in docker-compose
    
    * feat: implement user authentication with bcrypt and update user entity
    
    * fix: ensure password hash is converted to Buffer with utf-8 encoding
    
    ---------
    
    Co-authored-by: sam-mccarthy <110861054+sam-mccarthy@users.noreply.github.com>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
+import session from 'express-session';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LocalStrategy } from './auth/local.strategy';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    session({
+      secret: 'THIS SHOULD BE REPLACED WITH AN ENVIRONMENT KEY EVENTUALLY',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: false }, // should be true in production
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
