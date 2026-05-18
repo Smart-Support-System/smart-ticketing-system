efbd7af8059a10c4438f03993a65105e5215dcc4
sam-mccarthy <110861054+sam-mccarthy@users.noreply.github.com>
2026-05-18 17:14:42 -0400

    fix: hardcode backend url to avoid env var shenanigens in frontend

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -11,8 +11,8 @@ import type { Route } from "./+types/root";
 import process from "process";
 import "./app.css";
 
-const backend_host = import.meta.env.VITE_BACKEND_HOST ?? 'localhost';
-const backend_port = import.meta.env.VITE_BACKEND_PORT ?? 3000;
+const backend_host = 'localhost';
+const backend_port = 3000;
 export const API_BASE_URL = `${backend_host}:${backend_port}`;
 
 export const links: Route.LinksFunction = () => [
